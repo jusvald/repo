5373fba830e33535f125cbe8518c2b94f044d29e
jusvald vald <valderama.justin@iisgalvanimi.edu.it>
2024-05-08 16:15:13 +0000

    meteo

--- MiniMeteo/src/app/app.component.ts
+++ MiniMeteo/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniMeteo';
-  
+  title = 'minimeteo';
+  nome: any;
+  temp: any;
+  clouds: any;
+  desc: any;
+  feelslike: any;
+  apikey = '';
+  loading = false;
 
+  constructor() {}
 
+  ngOnInit() {}
+
+  prendiMeteo(citta: any) {
+    alert(citta);
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+citta+'&appid='+this.apikey+'60c08cb918438e418245a6d4ba83d492')
+    .then(response => response.json())
+    .then(data => {
+      this.temp = data['main']['temp'];
+      this.nome = data['name'];
+      this.desc = data['weather'][0]['description'];
+      this.clouds = data['clouds']['all'];
+      this.feelslike = data['main']['feels_like'];
+      this.loading = true;
+    })}
 }
--- MiniMeteojQuery/src/app/app.component.ts
+++ MiniMeteojQuery/src/app/app.component.ts
@@ -1,5 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
 
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'1f4fdda1a657f841c460708612bf600d')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Nome della città inesistente"));
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
